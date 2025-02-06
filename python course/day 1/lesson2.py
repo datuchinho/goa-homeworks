eee594ec70362eb87614da4ddef38145e1d3e742
datuchinho <datodobo22@gmail.com>
2025-02-06 22:19:00 +0400

    first commit

---
+++ python course/day 1/lesson2.py
@@ -0,0 +1,19 @@
+name ="davit"
+
+surname ="doborjginidze"
+
+print(name)
+print(surname)
+
+name= "davit"
+age = 15
+height=181
+
+knows_programming = False
+is_ugly=False
+
+
+
+
+
+print(name +" "+ surname +" "+ str(age) +" "+ str(height))
